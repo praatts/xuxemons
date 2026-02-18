9407203ebe18439ee64bec235d76d9d425f7e802
Marc <marcvalentinesmiro@gmail.com>
2026-02-18 19:16:34 +0100

    Uso de middleware y declaración de rutas correcto
    Cambio de función de registro a controlador AuthController

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -17,69 +17,6 @@ class UserController extends Controller
         return UserResource::collection($users);
     }
 
-    //registro usuarios
-    public function store(Request $request){
-        $request->validate([
-            'player_id' => [
-                'required',
-                'string',
-                'max:255',
-                'unique:users,player_id',
-                'regex:/^[A-Za-z]+[0-9]{4}$/'
-            ],
-
-            'name' => [
-                'required',
-                'string',
-                'max:255',
-            ],
-
-            'surname' => [
-                'required',
-                'string',
-                'max:255',
-            ],
-
-            'email' => [
-                'required',
-                'email',
-                'max:255',
-                'unique:users,email'
-            ],
-
-            'password' => [
-                'required',
-                'string',
-                'min:6'
-            ],
-
-            'role' => [
-                'required',
-                'in:user,admin'
-            ],
-        ]);
-
-        $user = User::create([
-            'player_id' => $request->player_id,
-            'name' => $request->name,
-            'surname' => $request->surname,
-            'email' => $request->email,
-            'password' => $request->password,
-            'role' => $request->role
-        ]);
-
-        try {
-            $token = JWTAuth::fromUser($user);
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'Could not create token'], 500);
-        }
-
-        return response()->json([
-            'token' => $token,
-            'user' => new UserResource($user)
-        ]);
-    }
-
     //mostrar perfil usuario
     public function getUser()
     {
