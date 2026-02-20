c1cc88862ac9dbc9cc6ac3e51d083fac95283e2f
oriol-valentines <oriolvalentinesmiro@gmail.com>
2026-02-20 19:57:16 +0100

    router link creado (falta css)

--- frontend/src/app/user-principal/user-principal.component.ts
+++ frontend/src/app/user-principal/user-principal.component.ts
@@ -1,12 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-principal',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, NgClass],
   templateUrl: './user-principal.component.html',
   styleUrl: './user-principal.component.css'
 })
 export class UserPrincipalComponent {
+  title = 'inicio';
+  content = 'welcome to ower principal page...'
+
+  botonInfoHover = false;
 
 }
