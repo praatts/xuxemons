ee26f4690a75d6826dd905abf309d02b2e85a1e0
oriol-valentines <oriolvalentinesmiro@gmail.com>
2026-02-20 20:23:11 +0100

    .

--- frontend/src/app/user-info/user-info.component.ts
+++ frontend/src/app/user-info/user-info.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.css'
 })
@@ -11,5 +13,8 @@ export class UserInfoComponent {
 
   title = 'user info page';
   text = 'This is the page that you can see your info';
+
+  constructor(){}
+  
 
 }
