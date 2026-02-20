ab57036638b0f32cdbcf0efa8430b45322da4e66
praatts <albertprba@gmail.com>
2026-02-20 17:31:02 +0100

    Conflictes del merge + errors arreglats

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\JwtMiddleware;
