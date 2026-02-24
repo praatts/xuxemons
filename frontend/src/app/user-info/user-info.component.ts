97943e6be5b82a98f7347eb36f6dc74029746a30
praatts <albertprba@gmail.com>
2026-02-24 19:59:19 +0100

    Mètodes auth service per carregar l'usuari

--- frontend/src/app/user-info/user-info.component.ts
+++ frontend/src/app/user-info/user-info.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-user-info',
@@ -14,8 +15,12 @@ export class UserInfoComponent {
   title = 'user info page';
   text = 'This is the page that you can see your info';
 
-  constructor(
-  ){}
+  constructor(authService: AuthService) {}
+
+  //Método que carga el usuario
+  loadProfile() {
+    
+  }
   
 
 }
