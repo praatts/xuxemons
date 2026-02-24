1dfc6779bc86d4511e64a454aa8498ddec00959d
praatts <albertprba@gmail.com>
2026-02-24 19:41:42 +0100

    Update JWT Middleware

--- backend/app/Http/Middleware/JwtMiddleware.php
+++ backend/app/Http/Middleware/JwtMiddleware.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Exception;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
 class JwtMiddleware
 {
@@ -20,9 +22,9 @@ class JwtMiddleware
         try{
             $user = JWTAuth::parseToken()->authenticate();
         } catch(Exception $e){
-            if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+            if($e instanceof TokenInvalidException){
                 return response()->json(['status' => 'Token is Invalid'], 401);
-            } else if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+            } else if($e instanceof TokenExpiredException){
                 return response()->json(['status' => 'Token is Expired'], 401);
             } else {
                 return response()->json(['status' => 'Authorization Token not found'], 401);
