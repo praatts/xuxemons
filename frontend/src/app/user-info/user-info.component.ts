67c664ec5c3fce5964996e493cdbf824c5c636fc
oriol-valentines <oriolvalentinesmiro@gmail.com>
2026-02-24 18:42:31 +0100

    merge

--- frontend/src/app/user-info/user-info.component.ts
+++ frontend/src/app/user-info/user-info.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, NgClass],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.css'
 })
@@ -15,7 +15,6 @@ export class UserInfoComponent {
   text = 'This is the page that you can see your info';
 
   constructor(
-    
   ){}
   
 
