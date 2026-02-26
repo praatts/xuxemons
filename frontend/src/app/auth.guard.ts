a1925546bf15ab102facfbf790d947b6c441a715
Marc <marcvalentinesmiro@gmail.com>
2026-02-26 19:12:11 +0100

    authGuard para proteger rutas

---
+++ frontend/src/app/auth.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const router = inject(Router);
+  const token = localStorage.getItem('authToken');
+  
+  //verificar que hay token
+  if (token) {
+    return true;
+  }
+
+  //si no hay token redirigir al login
+  router.navigate(['/']);
+  return false;
+};
