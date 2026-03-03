9cee2a99bdef8ad3826d3d822fef7fe8bd808ded
oriol-valentines <oriolvalentinesmiro@gmail.com>
2026-03-03 19:14:40 +0100

    migraciones echas y rellenadas

---
+++ backend/database/migrations/2026_03_03_181120_create_xuxes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('xuxes', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['agua', 'tierra', 'aire']);
+            $table->enum('size', ['pequeno', 'mediano', 'grande']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('xuxes');
+    }
+};
