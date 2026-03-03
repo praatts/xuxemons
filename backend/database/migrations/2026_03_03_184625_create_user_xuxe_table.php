7218ef9fc16f10848ed7dd010012c1f9f4fa97a8
oriol-valentines <oriolvalentinesmiro@gmail.com>
2026-03-03 19:54:47 +0100

    ultima migracion creada

---
+++ backend/database/migrations/2026_03_03_184625_create_user_xuxe_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    /* muy parecida a create_inventory_table pero sin limites, no hay maximo de filas ni de cantidad */
+    public function up(): void
+    {
+        Schema::create('user_xuxe', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('xuxe_id')->constrained('xuxes')->onDelete('cascade');
+            $table->usignedInteger('quantity')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_xuxe');
+    }
+};
