70a36bcc7d89fa3ac1d5bcb5ca1a902d16d08a77
oriol-valentines <oriolvalentinesmiro@gmail.com>
2026-03-04 18:53:08 +0100

    InventoryController Inicialicación

--- backend/app/Http/Controllers/API/InventoryController.php
+++ backend/app/Http/Controllers/API/InventoryController.php
@@ -5,7 +5,15 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Inventory;
+use App\Models\Xuxe;
+use Illuminate\Http\JsonResponse;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 class InventoryController extends Controller
 {
-    //
+    const MAX_SLOTS = 20;
+    const MAX_STACK = 5;
+
+    
 }
