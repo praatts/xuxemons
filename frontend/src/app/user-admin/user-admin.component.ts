bc3ceb64abf3517c9976131f2bbff31b0455c8b2
oriol-valentines <oriolvalentinesmiro@gmail.com>
2026-03-12 17:18:32 +0100

    logica panel administracion

--- frontend/src/app/user-admin/user-admin.component.ts
+++ frontend/src/app/user-admin/user-admin.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-admin.component.html',
   styleUrl: './user-admin.component.css'
 })
-export class UserAdminComponent {
+export class UserAdminComponent implements OnInit {
 
+  users: any[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getAllUsers().subscribe((data: any) => {
+      this.users = data;
+    });
+  }
+
+  restoreUser(id: number) {
+    this.userService.restoreUsers(id).subscribe(() => {
+      this.loadUsers();
+    });
+  }
 }
