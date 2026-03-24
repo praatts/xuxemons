a6957dc3dd50964b018a60d854678ecfc8d1b7aa
Marc <marcvalentinesmiro@gmail.com>
2026-03-24 17:12:29 +0100

    base para el formulario de modificación de daily xuxes...

--- frontend/src/app/admin-settings/admin-settings.component.ts
+++ frontend/src/app/admin-settings/admin-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-admin-settings',
@@ -8,5 +10,84 @@ import { Component } from '@angular/core';
   styleUrl: './admin-settings.component.css'
 })
 export class AdminSettingsComponent {
+
+  settingsForm: FormGroup;
+  msg = '';
+  showDeleteDialog = false;
+
+  little_to_midValue = '';
+  mid_to_bigValue = '';
+  daily_xuxes_quantityValue = '';
+  daily_xuxes_timeValue = '';
+
+  constructor(private settingsService: SettingsService) {
+    this.settingsForm = new FormGroup({
+      littleToMiddle: new FormControl('', [Validators.min(3), Validators.max(10)]),
+      middleToBig: new FormControl('', [Validators.min(5), Validators.max(15)]),
+      daylyXuxesQuantity: new FormControl('', [Validators.min(5), Validators.max(20)]),
+      dailyXuxesTime: new FormControl('',)
+    });
+  }
+
+  //VALIDACION DE ERRORES
+  getErrorMessage(controlName: string): string {
+    const control = this.settingsForm.get(controlName);
+
+    if (!control || !control.errors || !control.touched) return '';
+
+    const errors = control.errors;
+
+    if (errors['required']) return 'Este campo es obligatorio';
+    if (errors['time']) return 'Formato de email no válido';
+    if (errors['max']) return 'El email introducido ya está en uso';
+    if (errors['min']) return `El valor mínimo es ${errors['min'].min}`;
+
+    return 'Error de validación';
+  }
+
+  private timeToMinutes(time: string): number {
+    if (!time) return 0;
+
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+  timeFormatValidator() {
+    const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
+
+    return (control: any) => {
+      if (!control.value) return null;
+
+      return regex.test(control.value)
+        ? null
+        : { timeFormat: true };
+    };
+  }
+
+  minTime(minTime: string) {
+    return (control: any) => {
+      if (!control.value) return null;
+
+      const value = this.timeToMinutes(control.value);
+      const min = this.timeToMinutes(minTime);
+
+      return value < min
+        ? { minTime: { min: minTime } }
+        : null;
+    };
+  }
+
+  maxTime(maxTime: string) {
+    return (control: any) => {
+      if (!control.value) return null;
+
+      const value = this.timeToMinutes(control.value);
+      const max = this.timeToMinutes(maxTime);
+
+      return value > max
+        ? { maxTime: { max: maxTime } }
+        : null;
+    };
+  }
 
 }
