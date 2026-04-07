fc3f57acc506fd97658a5ea720160f36f7cc5ba1
praatts <albertprba@gmail.com>
2026-04-07 20:05:37 +0200

    Comentaris user-login component

--- frontend/src/app/user-login/user-login.component.ts
+++ frontend/src/app/user-login/user-login.component.ts
@@ -27,7 +27,7 @@ export class UserLoginComponent {
     private authService: AuthService
   ) {
 
-    // Creamos el formulario reactivo
+    // Creació del formulari reactiu per al login
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
@@ -35,30 +35,31 @@ export class UserLoginComponent {
 
   }
 
+  //Mètode per enviar les dades al login
   onSubmit() {
-
     this.loading = true;
     this.errorMessage = '';
     const email = this.loginForm.value.email;
     const password = this.loginForm.value.password;
 
-    // Si el formulario es inválido, no continuamos
+    // Si el formulari és invàlid, no fem la crida per fer el login
     if (this.loginForm.invalid) {
       return;
     }
 
+    //Fem el login amb els valors introduïts al formulari, i gestionem la resposta del backend per mostrar un missatge d'error o redirigir a la pàgina principal segons el resultat de l'operació.
     this.userService.logIn(email, password).subscribe({
 
       next: (response) => {
-        console.log('Respuesta recibida en componente:', response);
+        console.log('Resposta rebuda al component:', response);
         this.loading = false;
-        // Forzamos la navegación
-        this.router.navigateByUrl('/main/principal'); 
+        // Forcem la navegació a la pàgina principal després del login.
+        this.router.navigateByUrl('/main/principal'); //Redirigim ja que el backend retorna 200 en canvi de 401 (s'ha de canviar)
       },
       error: (error) => {
         this.loading = false;
         console.error('Error capturado:', error);
-        this.errorMessage = error.status === 401 ? 'Credenciales incorrectas.' : 'Error al iniciar sesión.';
+        this.errorMessage = error.status === 401 ? 'Credenciales incorrectas.' : 'Error al iniciar sesión.'; //Mostra missatge d'error específic si les credencials son incorrectes.
       }
     });
   }
