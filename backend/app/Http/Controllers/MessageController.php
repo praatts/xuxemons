5a5c4f69462d2a169a0f7180f4df7ae568d3e013
praatts <127567514+praatts@users.noreply.github.com>
2026-04-16 21:23:18 +0200

    Mètode per carregar missatges conversa específica + rutes API

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -10,6 +10,7 @@ use Auth;
 
 class MessageController extends Controller
 {
+    //Funció per enviar un missatge en una conversa existent
     public function store(Request $request)
     {
 
@@ -45,4 +46,35 @@ class MessageController extends Controller
 
         return response()->json();
     }
+
+    //Funció per obtenir els missatges d'una conversa
+    public function index(Request $request) {
+        $request->validate([
+            'conversation_id' => 'required|exists:conversations,id',
+        ]);
+
+        //Obtenim usuari autenticat i id de la conversa
+        $user = Auth::guard('api')->user();
+        $conversation_id = $request->input('conversation_id');
+
+        //Carreguem la conversa
+        $conversation = Conversation::find($conversation_id);
+
+        //Comprovem que la conversa existeix
+        if (!$conversation) {
+            return response()->json(['error' => 'Conversa no trobada'], 404);
+        }
+
+        //Comprovem que l'usuari autenticat és part de la conversa (o sender o receiver)
+        if ($conversation->sender_id !== $user->id && $conversation->receiver_id !== $user->id) {
+            return response()->json(['error' => 'No tens permís per veure els missatges d\'aquesta conversa'], 403);
+        }
+
+        //Carreguem els missatges de la conversa de manera ordenada per data de creació (utilitzant la relació definida al model Conversation)
+        $messages =  $conversation->messages()->orderBy('created_at', 'asc')->get();
+       
+        return response()->json($messages);
+    }
+
+    
 }
