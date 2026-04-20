163d2b4c260dc053a704ce283c7e4f052817a25c
praatts <127567514+praatts@users.noreply.github.com>
2026-04-20 22:20:21 +0200

    Creació battle service + interfaces necessàries
    Interface d'usuari bàsica per aquesta situació, no necessitem més que la id i el nom, ja que el camp 'player_one|two_id es qui determina la lògica

---
+++ frontend/src/app/services/battle.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Battle } from '../../../interfaces/battle';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BattleService {
+
+  constructor() { }
+}
