2bf25737ae546ba31844128bac66daa47d1221b5
praatts <127567514+praatts@users.noreply.github.com>
2026-04-20 22:25:16 +0200

    Servei Battle (crides endpoint + BehaviorSubject)

--- frontend/src/app/services/battle.service.ts
+++ frontend/src/app/services/battle.service.ts
@@ -8,5 +8,36 @@ import { Battle } from '../../../interfaces/battle';
 })
 export class BattleService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8000/api';
+  private battlesSubject = new BehaviorSubject<Battle[]>([]);
+  public battles$;
+
+  constructor(private http: HttpClient) {
+    this.battles$ = this.battlesSubject.asObservable();
+  }
+
+  //Mètode que carrega les battalles de l'usuari autenticat.
+  loadBattles() {
+    return this.http.get<Battle[]>(`${this.apiUrl}/battles`);
+  }
+
+  //Esctirua llista de batalles emesa pel BehaviorSubject.
+  setBattles(battles: Battle[]) {
+    this.battlesSubject.next(battles);
+  }
+
+  //Lectura dels valors del BehaviorSubject de batalles.
+  getBattles() : Battle[] {
+    return this.battlesSubject.value;
+  }
+
+  //Mètode per acceptar una batalla pendent, actualitzant el seu estat a "accepted" i permetent iniciar la batalla.
+  acceptBattle(battle_id: number) {
+    return this.http.post(`${this.apiUrl}/battles/${battle_id}/accept`, {});
+  }
+
+  //Mètode que inicia la batalla
+  fightBattle(battle_id: number) {
+    return this.http.post(`${this.apiUrl}/battles/${battle_id}/fight`, {});
+  }
 }
